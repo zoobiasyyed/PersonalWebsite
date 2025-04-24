7e1900ac6fa34e83ccb72af2b05281ef425081d1
Zoobia Syyed <syyedzoobia@gmail.com>
2025-04-24 15:02:36 -0700

    initial set up done

---
+++ my-portfolio/src/about-me.tsx
@@ -0,0 +1,7 @@
+export default function AboutMe(){
+  return(
+    <>
+      <h2>this is about me</h2>
+    </>
+  )
+}
---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <div>Welcome to my personal website...</div>
+  );
+}
