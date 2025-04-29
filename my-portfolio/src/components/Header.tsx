2d188da18e413eb3b30f2f2c57e3d65ac01017a6
Zoobia Syyed <syyedzoobia@gmail.com>
2025-04-29 15:26:49 -0700

    playing around...

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <header className="w-full flex justify-center py-4 bg-white">
-        <h1 className="text-3xl font-bold center">Hi! I'm Zoobia</h1>
+        <h1 className="text-3xl font-bold center">Hi! I&apos;m Zoobia</h1>
       </header>
       <nav>
         <button>About Me</button>
